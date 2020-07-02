b6d20a1f371f86275443189868ca7cd5791ea7f7
Jhoss <kuro.rc10@gmail.com>
2020-07-02 14:20:49 -0500

    Desarrollando el Primer Ejercicio

---
+++ Tema 01/tema-01-ejercicios.sql
@@ -0,0 +1,34 @@
+
+/*
+	EJERCICIO 1
+
+Se asignó la tarea de implementar una Base de datos a la
+empresa Recobra con un datafile y un logfile ubicados en la
+unidad C:\ carpeta Files, con tamaño inicial de 5MB hasta un
+máximo de 50MB con un factor de crecimiento de 5MB, para
+ambos archivos.
+
+*/
+
+USE master
+GO
+
+CREATE DATABASE BD_RECOBRA
+ON (
+  NAME = RECOBRA_DATA,
+  FILENAME = 'C:\Files\RECOBRA_DATA.MDF',
+  SIZE = 5MB,
+  MAXSIZE = 50MB,
+  FILEGROWTH = 5MB
+)
+LOG ON (
+  NAME = RECOBRA_LOG,
+  FILENAME = 'C:\Files\RECOBRA_LOG.LDF',
+  SIZE = 5MB,
+  MAXSIZE = 50MB,
+  FILEGROWTH = 5MB
+)
+GO
+
+SP_HELPDB BD_RECOBRA
+GO
