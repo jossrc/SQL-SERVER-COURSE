946ff618f89ee39e603e3f6e8fb4bc25ef51648a
Jhoss <kuro.rc10@gmail.com>
2020-07-02 14:41:26 -0500

    Realizando la Segunda pregunta del Experimentar

--- Tema 01/experimentar-01.sql
+++ Tema 01/experimentar-01.sql
@@ -8,3 +8,8 @@ GO
 
 SELECT name, user_access_desc, is_read_only, state_desc, recovery_model_desc  
 FROM sys.databases;
+
+-- 02. Crear una base de datos con valores predeterminados llamada BDClaro
+
+CREATE DATABASE BDClaro
+GO
