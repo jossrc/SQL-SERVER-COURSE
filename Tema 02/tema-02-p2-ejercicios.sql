5fa8b9046dfac60885c92b21d3cdc6b6fcd384b2
Jhoss <kuro.rc10@gmail.com>
2020-07-03 14:47:39 -0500

    Iniciando con los ejercicios del Tema 2 Parte II

---
+++ Tema 02/tema-02-p2-ejercicios.sql
@@ -0,0 +1,27 @@
+/*
+
+En la empresa Recobra, se decidió implementar una Tabla
+particionada para almacenar sus solicitudes y poder optimizar
+las consultas por trimestre de cualquier año, por un tema de
+análisis comparativo.
+
+*/
+
+-- 1. Crear los filegroups.
+
+
+
+-- 2. Adicionar los datafiles.
+
+
+
+-- 3. Crear la Función de Partición.
+
+
+
+-- 4. Crear el esquema de Partición.
+
+
+
+-- 5. Crear la tabla Particionada
+
