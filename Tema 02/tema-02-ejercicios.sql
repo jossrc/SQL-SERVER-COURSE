6b79be7da4ba8ca414f0d348dcd3dcc8f0b2fc61
Jhoss <kuro.rc10@gmail.com>
2020-07-03 11:15:45 -0500

    Iniciando con los ejercicios del Tema 02 Parte I

---
+++ Tema 02/tema-02-ejercicios.sql
@@ -0,0 +1,21 @@
+/*
+	EJERCICIO 1
+En la empresa Recobra, se decidió implementar schemas para
+una mejor organización de sus objetos , estos son: VENTAS,
+RRHH y FINANZAS.
+
+*/
+
+
+
+
+
+/*
+	EJERCICIO 2
+Por un tema de estándar, en la empresa Recobra, se
+implementará tipos de datos definidos por el usuario que serán:
+  + TCadena basado en varchar(50).
+  + TMoneda basado en smallmoney.
+  + TTiempo basado en date.
+
+*/
