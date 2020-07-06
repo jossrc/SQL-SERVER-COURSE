dbb2a9d5eeb92cb6b6187f685ed06b4f0c0c2d84
Jhoss <kuro.rc10@gmail.com>
2020-07-06 15:53:16 -0500

    Desarrollando la tercera pregunta

--- Tema 03/experimentar-03.sql
+++ Tema 03/experimentar-03.sql
@@ -26,6 +26,40 @@ GO
 
 -- 03. Crear las siguientes tablas, en los esquemas indicados
 
+CREATE TABLE SERVICIO.TBMedico (
+  idMed    CHAR(5)      NOT NULL,
+  nomMed   VARCHAR(50)  NOT NULL,
+  apMed    VARCHAR(50)  NOT NULL,
+  espMed   VARCHAR(50)  NOT NULL,
+  colMed   CHAR(12)     NOT NULL
+)
+GO
+
+CREATE TABLE USUARIO.TBPaciente (
+  idPac    CHAR(5)      NOT NULL,
+  nomPac   VARCHAR(50)  NOT NULL,
+  apPac    VARCHAR(50)  NOT NULL,
+  fnaPac   DATE         NOT NULL,
+  fonoPac  VARCHAR(10)  NOT NULL
+)
+GO
+
+CREATE TABLE SERVICIO.TBReceta (
+  numRec   INT      NOT NULL IDENTITY(1001,1),
+  fecRec   DATE     NOT NULL,
+  idPac    CHAR(5)  NOT NULL,
+  idMed    CHAR(5)  NOT NULL
+)
+GO
+
+CREATE TABLE SERVICIO.TBDetalleReceta (
+  numRec       INT          NOT NULL,
+  codMedicina  CHAR(5)      NOT NULL,
+  canti        TINYINT      NOT NULL,
+  dosis        VARCHAR(50)  NOT NULL,
+  indica       VARCHAR(50)  NOT NULL
+)
+GO
 
 -- 04. Crear las siguientes restricciones
 
