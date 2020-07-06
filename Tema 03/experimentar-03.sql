b80f5506194cbf0d9f4ac37a8274f4aad7b7d1c0
Jhoss <kuro.rc10@gmail.com>
2020-07-06 15:31:25 -0500

    Iniciando el Experimentar 03

---
+++ Tema 03/experimentar-03.sql
@@ -0,0 +1,28 @@
+USE master
+GO
+
+/*
+   01. Cree la base de datos BDClinica con valores de propiedades predeterminadas,
+      validando su existencia, eliminándola si existiera.
+*/
+
+
+
+-- 02. Abrir la base de datos y crear los siguientes esquemas
+
+
+
+-- 03. Crear las siguientes tablas, en los esquemas indicados
+
+
+-- 04. Crear las siguientes restricciones
+
+
+/*
+   05. Crear los siguientes índices:
+     a. Cree un Índice único compuesto (fecha y idpaciente de RECETA) ordenado
+        de forma ascendente.
+     b. Cree un Índice normal compuesto (nombre y apellidos del paciente)
+        ordenado de forma descendente para ambos campos.
+     c. Cree un Índice normal (apellido del médico), incluir el campo especialidad.
+*/
