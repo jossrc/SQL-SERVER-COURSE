ba188fcd88ee21399130c1fc7d531d0c676a5ff4
Jhoss <kuro.rc10@gmail.com>
2020-07-06 14:57:46 -0500

    Iniciando los ejercicios del Tema 03

---
+++ Tema 03/tema-03-ejercicios.sql
@@ -0,0 +1,12 @@
+/*
+
+En la empresa Recobra, se decidió implementar restricciones a
+la tabla Empleado que se creará en el schema RRHH.
+   + Llave primaria NonClustered.
+   + Nombre y apellidos debe ser únicos.
+   + Sueldo no debe ser menor de 1000, ni tampoco exceder los
+     15000.
+   + Valor predeterminado fecha de ingreso con fecha actual del
+     sistema.
+
+*/
