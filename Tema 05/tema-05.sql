02cc6c63fee767133557ccd5313ef895122904b3
Jhoss <kuro.rc10@gmail.com>
2020-07-07 11:25:28 -0500

    Inserción múltiples filas

--- Tema 05/tema-05.sql
+++ Tema 05/tema-05.sql
@@ -6,3 +6,12 @@ GO
 INSERT Compras.categorias (IdCategoria, NombreCategoria) 
   VALUES (99, 'Cereales')
 GO
+
+-- INSERCIÓN MÚLTIPLES FILAS
+
+INSERT INTO Compras.categorias (IdCategoria, NombreCategoria)
+  SELECT CategoryID, CategoryName FROM Northwind.dbo.Categories
+GO
+
+SELECT * FROM Compras.categorias
+GO
