36d8172a09adf3cdd5f1931702bbbed2b22d8141
Jhoss <kuro.rc10@gmail.com>
2020-07-15 14:19:24 -0500

    Iniciando el experimentar 5b

---
+++ Tema 05/experimentar-5b.sql
@@ -0,0 +1,100 @@
+-- EXPERIMENTAR 5B
+
+-- 1. ¿Cómo se halla la longitud de una cadena en un campo de una tabla?
+
+
+-- 2. ¿Cómo se hallan los nombres de los vendedores con más de 4 letras?
+
+
+-- 3. ¿Cómo se hallan los vendedores cuyos apellidos finaliza en O, N o S?
+
+
+-- 4. ¿Cómo se lista el primer carácter del nombre y apellido de los vendedores?
+
+
+-- 5. ¿Cómo se listan los productos, donde el tercer y cuarto carácter sea "PE"?
+
+
+-- 6. ¿Cómo se listan los clientes donde el cuarto y quinto carácter sea "02"?
+
+
+-- 7. ¿Cómo se convierte una cadena en minúscula (descripción de producto)?
+
+
+-- 8. ¿Cómo se convierte una cadena en mayúscula (apellido del vendedor)?
+
+
+-- 9. ¿Cómo se reemplaza una cadena por otra cadena?
+
+
+-- 10. ¿Cómo se reemplaza parte de una cadena y agrego otra?
+
+
+-- 11. ¿Cómo se muestra una cadena impresa al revés?
+
+
+-- 12. ¿Cómo se repite varias veces una cadena?
+
+
+-- 13. ¿Cómo se obtiene el absoluto de un número?
+
+
+-- 14. ¿Cómo se eleva un número a una potencia?
+
+
+-- 15. ¿Cómo se redondea al número entero mayor más próximo?
+
+
+-- 16. ¿Cómo se redondea al número entero menor más próximo?
+
+
+-- 17. ¿Cómo se redondea un número indicándole la cantidad de decimales a
+--      considerar?
+
+
+-- 18. ¿Cómo se convierte un tipo de número con 8 decimales a otro, de 2?
+
+
+-- 19. ¿Cómo se imprime un mensaje en la consola del SQL Server 2014?
+
+
+-- 20. ¿Cómo se da formato DD/MM/YY al campo fecha de una tabla?
+
+
+-- 21. ¿Cómo se da formato DD/MM/YYYY al campo fecha de una tabla?
+
+
+-- 22. ¿Cómo se obtiene el día de la semana de hoy?
+
+
+-- 23. ¿Cómo se obtiene el mes de hoy?
+
+
+-- 24. ¿Cómo se obtiene el día de facturación?
+
+
+-- 25. ¿Cómo se obtiene el día en que nací?
+
+
+-- 26. ¿Cómo se obtienen los clientes registrados un lunes o viernes?
+
+
+-- 27. ¿Cómo se obtiene el año de registro de los clientes?
+
+
+-- 28. ¿Cómo se obtienen las órdenes de compra registradas entre enero y junio?
+
+
+-- 29. ¿Cómo se obtiene el número del día de una fecha?
+
+
+-- 30. ¿Cómo se obtienen las facturas emitidas un sábado del primer semestre del 2010?
+
+
+-- 31. ¿Cómo se obtiene el día si le adiciono 25 días a la fecha actual?
+
+
+-- 32. ¿Cómo se determina la cantidad de días transcurridos para la cancelación de una
+--     factura?
+
+
