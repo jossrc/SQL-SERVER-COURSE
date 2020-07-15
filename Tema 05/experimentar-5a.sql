666d2632ac7170441d066112c935a3195094df3c
Jhoss <kuro.rc10@gmail.com>
2020-07-15 14:13:55 -0500

    Iniciando el experimentar 5a

---
+++ Tema 05/experimentar-5a.sql
@@ -0,0 +1,70 @@
+-- EXPERIMENTAR 5A
+
+-- 1. Liste el código, razón social y RUC de los clientes.
+
+
+-- 2. Liste el código, descripción y el stock de los productos en orden descendente por
+--    precio.
+
+
+-- 3. Modifique la consulta anterior para ordenar descendente por precio y ascendente
+--    stock.
+
+
+-- 4. Liste el código y descripción de productos por el campo LIN_PRO con valor 1 o 3.
+
+
+-- 5. Modifique la consulta aplicando el operador IN.
+
+
+-- 6. Liste la razón social y dirección de proveedores de los distritos D02, D04, D06 o
+--    D09.
+
+
+-- 7. Liste la razón social y teléfonos de los clientes del distrito D02 del tipo 1.
+
+
+-- 8. Liste la descripción, precio y stock de productos con precio entre 10 y 20 o stock <
+--    50.
+
+
+-- 9. Modifique la consulta, sin emplear BETWEEN.
+
+
+-- 10. Liste el número de factura y fecha de facturación de las facturas generadas entre 2
+--     fechas.
+
+
+-- 11. Liste razón social y dirección de proveedores con teléfono de la serie 445.
+
+
+-- 12. Liste los productos donde segunda letra de descripción tenga una "A" o una "O".
+
+
+-- 13. Modifique la consulta anterior para obtener los productos cuya descripción tenga
+--    "AP".
+
+
+-- 14. Liste las facturas cuya fecha de cancelación aun no se haya definido.
+
+
+-- 15. Liste los números de factura, fecha de facturación y estado de facturas emitidas en
+--     el 2010.
+
+
+-- 16. Liste la razón social de clientes indicando su antiguedad en años.
+
+
+-- 17. Modifique la consulta anterior para seleccionar clientes cuya antiguedad sea > a 8
+--     años.
+
+
+-- 18. Liste los clientes registrados en la primera quincena del último trimestre del 2005.
+
+
+-- 19. Liste de manera única los códigos del vendedor que han generado ventas en el
+--     2010.
+
+
+-- 20. Liste la razón social y RUC de los 2 clientes más antiguos.
+
