9802ffa3b01b1b9c7ac417a6e7887f5b478a40d1
Jhoss <kuro.rc10@gmail.com>
2020-07-15 15:16:51 -0500

    Utilizando la clausula ORDER BY

--- Tema 06/tema-06.sql
+++ Tema 06/tema-06.sql
@@ -12,10 +12,30 @@ SELECT *
 FROM Ventas.clientes
 GO
 
--- MOSTRAR CIERTOS CAMPOS
+-- MOSTRAR CIERTOS CAMPOS CON ALIAS
 
 SELECT IdCliente  AS 'Código',
        NomCliente AS 'Nombre del Cliente',
        DirCliente AS 'Dirección'
 FROM Ventas.clientes
+GO
+
+-- ORDER BY
+
+SELECT *
+FROM Compras.productos
+ORDER BY IdCategoria ASC
+GO
+
+SELECT *
+FROM Compras.productos
+ORDER BY IdCategoria ASC,
+         IdProveedor DESC
+GO
+
+SELECT *
+FROM Compras.productos
+ORDER BY IdCategoria ASC,
+         IdProveedor DESC,
+         NomProducto ASC
 GO
