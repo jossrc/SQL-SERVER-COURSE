348ea7d6a5675c1e30dba3c6551956d7054e880e
Jhoss <kuro.rc10@gmail.com>
2020-07-15 13:32:59 -0500

    LEFT JOIN y RIGHT JOIN

--- Tema 05/tema-05-rd-02.sql
+++ Tema 05/tema-05-rd-02.sql
@@ -1,6 +1,8 @@
 USE Negocios
 GO
 
+-- INNER JOIN
+
 SELECT C.NomCliente, P.NomProveedor
 FROM Ventas.clientes AS C
 INNER JOIN Compras.proveedores AS P
@@ -12,4 +14,22 @@ SELECT PR.IdProducto, PR.NomProducto,
 FROM Compras.proveedores PV
 JOIN Compras.productos PR
   ON PV.IdProveedor = PR.IdProveedor
+GO
+
+-- LEFT JOIN
+
+SELECT CL.IdCliente, CL.NomCliente,
+       PC.IdPedido, PC.FechaPedido
+FROM Ventas.clientes AS CL
+LEFT JOIN Ventas.pedidoscabe AS PC
+  ON CL.IdCliente = PC.IdCliente
+GO
+
+-- RIGHT JOIN
+
+SELECT CL.IdCliente, CL.NomCliente,
+       PC.IdPedido, PC.FechaPedido
+FROM Ventas.clientes AS CL
+RIGHT JOIN Ventas.pedidoscabe AS PC
+  ON CL.IdCliente = PC.IdCliente
 GO
