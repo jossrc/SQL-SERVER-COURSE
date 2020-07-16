5e59159543b8e0d6bb6409e23e7a86cea5432fb5
Jhoss <kuro.rc10@gmail.com>
2020-07-16 11:13:36 -0500

    Iniciando los ejercicios del Tema 06

---
+++ Tema 06/tema-06-ejercicios.sql
@@ -0,0 +1,16 @@
+USE Negocios
+GO
+
+/*
+  01. Se requiere consultar los empleados con mas de 5 años de
+      antigüedad, debiendo mostrar los campos nombre, apellidos,
+      fecha de contratación. Utilice alias a los campos.
+*/
+
+
+
+
+/*
+    02. Consultar los 02 productos de las categorías 1,3 y 5 con mayor
+        unidades en existencia .
+*/
