8eb88f60a693470e1b674833597b00110c4637ed
Jhoss <kuro.rc10@gmail.com>
2020-07-16 11:25:23 -0500

    Desarrollando la primera pregunta

--- Tema 06/tema-06-ejercicios.sql
+++ Tema 06/tema-06-ejercicios.sql
@@ -2,13 +2,17 @@ USE Negocios
 GO
 
 /*
-  01. Se requiere consultar los empleados con mas de 5 años de
+  01. Se requiere consultar los empleados con mas de 9 años de
       antigüedad, debiendo mostrar los campos nombre, apellidos,
       fecha de contratación. Utilice alias a los campos.
 */
 
-
-
+SELECT NomEmpleado AS [Nombre del Empleado],
+       ApeEmpleado AS [Apellido del Empleado],
+       FecContrata AS [Fecha de contratación]
+FROM RRHH.empleados
+WHERE DATEDIFF(YY, FecContrata, GETDATE()) > 9
+GO
 
 /*
     02. Consultar los 02 productos de las categorías 1,3 y 5 con mayor
