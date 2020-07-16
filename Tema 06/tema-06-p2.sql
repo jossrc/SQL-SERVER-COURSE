c05d27cb2d22f9ace45f52482a20c15f3e9725e5
Jhoss <kuro.rc10@gmail.com>
2020-07-16 11:42:27 -0500

    Inner join

--- Tema 06/tema-06-p2.sql
+++ Tema 06/tema-06-p2.sql
@@ -1,3 +1,37 @@
 USE Negocios
 GO
 
+-- INNER JOIN
+
+SELECT PR.IdProducto,
+       PR.NomProducto,
+       PV.NomProveedor,
+       PR.PrecioUnidad
+FROM Compras.proveedores AS PV
+INNER JOIN Compras.productos AS PR
+  ON PV.IdProveedor = PR.IdProveedor
+GO
+
+SELECT PR.IdProducto,
+       PR.NomProducto,
+       PV.NomProveedor,
+       CA.NombreCategoria,
+       PR.PrecioUnidad
+FROM Compras.proveedores AS PV
+INNER JOIN Compras.productos AS PR
+  ON PV.IdProveedor = PR.IdProveedor
+INNER JOIN Compras.categorias AS CA
+  ON PR.IdCategoria = CA.IdCategoria
+GO
+
+SELECT PR.IdProducto,
+       PR.NomProducto,
+       PV.NomProveedor,
+       CA.NombreCategoria,
+       PR.PrecioUnidad
+FROM Compras.proveedores AS PV
+JOIN Compras.productos AS PR
+  ON PV.IdProveedor = PR.IdProveedor
+JOIN Compras.categorias AS CA
+  ON PR.IdCategoria = CA.IdCategoria
+GO
