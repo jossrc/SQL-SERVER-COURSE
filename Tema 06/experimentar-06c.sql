a6adb0b9fe9242393be6921ac6833d5f4b921f45
Jhoss <kuro.rc10@gmail.com>
2020-07-17 12:13:38 -0500

    Iniciando el Experimentar 06 C

---
+++ Tema 06/experimentar-06c.sql
@@ -0,0 +1,26 @@
+USE VENTASCIB
+GO
+
+/*
+**  Establezca una relación recursiva, es decir, una tabla relacionada 
+    a sí misma.
+*/
+
+-- a) Cree una copia de la tabla Tb_Vendedor.
+
+
+-- b) Agregue la columna Cod_Jefe a TB_Vendedor2.
+
+
+-- c) Establezca la relación recursiva, cod_jefe con PK de TB_Vendedor2 (Cod_Ven)
+
+
+-- d) Actualice los códigos de jefes de los vendedores.
+
+
+
+-- 1. Liste nombres y apellidos de vendedores con el nombre y apellido tengan o no
+--    jefes.
+
+
+-- 2. Liste nombres y apellidos de los empleados que no tienen jefe.
