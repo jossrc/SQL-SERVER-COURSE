b25eb34814c14373af951917eb6dc85463cac747
Jhoss <kuro.rc10@gmail.com>
2020-07-17 12:07:12 -0500

    Iniciando el Experimentar 06 A

---
+++ Tema 06/experimentar-06a.sql
@@ -0,0 +1,51 @@
+USE VENTASCIB
+GO
+
+-- 1. Liste la sumatoria de precios de los productos.
+
+
+-- 2. Liste el máximo y mínimo valor del sueldo de vendedores de tipo “1”.
+
+
+-- 3. Liste el promedio de antigüedad de los clientes.
+
+
+-- 4. Liste la cantidad de facturas pendientes.
+
+
+-- 5. Liste la cantidad de facturas generados por cliente.
+
+
+-- 6. Liste, en base al ejercicio anterior, los clientes con más de una factura.
+
+
+-- 7. Liste en base al ejercicio anterior las facturas del año 2010.
+
+
+-- 8. Liste los códigos de proveedores que abastecen más de 2 productos.
+
+
+-- 9. Liste el promedio de tiempo de servicio y sueldos por cada tipo de vendedor.
+
+
+-- 10. Liste la cantidad de facturas que han tardado más de 5 días en cancelarse.
+
+
+-- 11. Liste la razón social y ruc de clientes con una antigüedad mayor a la antigüedad
+--     promedio.
+
+
+-- 12. Liste los nombres y apellidos de vendedores que no ha generado facturas.
+
+
+-- 13. Liste la cantidad de facturas generadas por los vendedores del distrito D09.
+
+
+-- 14. Liste los productos comercializados en la factura FA002.
+
+
+-- 15. Liste la descripción y precio de los productos cuyo precio supera el precio
+--     promedio.
+
+
+
