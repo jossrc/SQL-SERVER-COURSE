5d2d7fe3b4d1d877400711bda6d9b2bdaf52fc8c
Jhoss <kuro.rc10@gmail.com>
2020-07-17 11:50:40 -0500

    Desarrollando la primera pregunta

--- Tema 06/tema-06-p2-ejercicios.sql
+++ Tema 06/tema-06-p2-ejercicios.sql
@@ -6,7 +6,16 @@ GO
       año, totalizando la cantidad de pedidos por cada año.
 */
 
-
+SELECT ISNULL(E.NomEmpleado, 'SUBTOTAL') AS [EMPLEADO],
+       YEAR(P.FechaPedido) AS [AÑO],
+       COUNT(E.IdEmpleado) AS [PEDIDOS]
+FROM Ventas.pedidoscabe AS P
+JOIN RRHH.empleados AS E
+  ON  P.IdEmpleado = E.IdEmpleado
+GROUP BY YEAR(P.FechaPedido), E.NomEmpleado
+WITH CUBE
+ORDER BY 2,3
+GO
 
 /*
   02. Muestre los clientes que no realizaron pedido alguno.
