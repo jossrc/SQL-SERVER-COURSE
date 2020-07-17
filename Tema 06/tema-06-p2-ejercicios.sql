c42c7d9bbe5ffa21ed5d516b9641800935ac3840
Jhoss <kuro.rc10@gmail.com>
2020-07-17 11:20:56 -0500

    Iniciando los ejercicios del Tema 06 Parte II

---
+++ Tema 06/tema-06-p2-ejercicios.sql
@@ -0,0 +1,13 @@
+USE Negocios
+GO
+
+/*
+  01. Listar la cantidad de pedidos registrados por cada empleado y
+      año, totalizando la cantidad de pedidos por cada año.
+*/
+
+
+
+/*
+  02. Muestre los clientes que no realizaron pedido alguno.
+*/
