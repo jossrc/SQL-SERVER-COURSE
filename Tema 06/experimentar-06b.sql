7e2502857dbbe6c9c4237cc5845436b526b454b8
Jhoss <kuro.rc10@gmail.com>
2020-07-17 12:09:37 -0500

    Iniciando el Experimentar 06 B

---
+++ Tema 06/experimentar-06b.sql
@@ -0,0 +1,37 @@
+USE VENTASCIB
+GO
+
+-- 1. Liste los datos del cliente y el nombre de su distrito.
+
+
+-- 2. Liste los números de factura y el total facturado.
+
+
+-- 3. Liste el número de la orden de compra, fecha, razón social, dirección y el distrito.
+
+
+-- 4. Liste la cantidad vendida y producto comercializados en la factura FA009.
+
+
+-- 5. Liste razón social, dirección, representante, precio de abastecimiento y distrito de
+--    todos los proveedores que abastecen el producto P002.
+
+
+-- 6. Liste el código de cliente, razón social y distrito (incluir distritos donde no existen
+--    clientes)
+
+
+-- 7. Liste los distritos donde no existen clientes (utilice LEFT OUTER JOIN).
+
+
+-- 8. Liste los distritos donde no existen clientes (utilice RIGHT OUTER JOIN).
+
+
+-- 9. Liste los proveedores (razón social y dirección), sin órdenes de compra.
+
+
+-- 10. Liste los clientes que no han generado facturas.
+
+
+-- 11. Liste la cantidad de clientes sin facturas.
+
