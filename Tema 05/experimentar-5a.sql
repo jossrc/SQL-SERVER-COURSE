ef8a2c8165c8bc12550df46cfaffff8d859c3e2a
Jhoss <kuro.rc10@gmail.com>
2020-07-18 10:25:21 -0500

    Resolviendo las preguntas del 06 al 10 del Experimentar

--- Tema 05/experimentar-5a.sql
+++ Tema 05/experimentar-5a.sql
@@ -41,20 +41,45 @@ GO
 -- 6. Liste la razón social y dirección de proveedores de los distritos D02, D04, D06 o
 --    D09.
 
+SELECT P.RAZ_SOC_PRV, P.DIR_PRV
+FROM dbo.TB_PROVEEDOR AS P
+WHERE P.COD_DIS IN ('D02','D04','D06', 'D09')
+GO
 
 -- 7. Liste la razón social y teléfonos de los clientes del distrito D02 del tipo 1.
 
+SELECT C.RAZ_SOC_CLI, C.TLF_CLI
+FROM dbo.TB_CLIENTE AS C
+WHERE C.COD_DIS = 'D02' AND C.TIP_CLI = 1
+GO
 
 -- 8. Liste la descripción, precio y stock de productos con precio entre 10 y 20 o stock <
 --    50.
 
+SELECT PR.DES_PRO, PR.PRE_PRO, PR.STK_ACT
+FROM dbo.TB_PRODUCTO AS PR
+WHERE (PR.PRE_PRO BETWEEN 10 AND 20) OR 
+       PR.STK_ACT < 50
+GO
 
 -- 9. Modifique la consulta, sin emplear BETWEEN.
 
+SELECT PR.DES_PRO, PR.PRE_PRO, PR.STK_ACT
+FROM dbo.TB_PRODUCTO AS PR
+WHERE (PR.PRE_PRO >= 10 AND PR.PRE_PRO <= 20) OR
+      PR.STK_ACT < 50
+GO
 
 -- 10. Liste el número de factura y fecha de facturación de las facturas generadas entre 2
 --     fechas.
 
+DECLARE @FEC1 DATE = '2013-05-01'
+DECLARE @FEC2 DATE = '2013-08-02'
+
+SELECT F.NUM_FAC, F.FEC_FAC
+FROM dbo.TB_FACTURA AS F
+WHERE F.FEC_FAC BETWEEN @FEC1 AND @FEC2
+GO
 
 -- 11. Liste razón social y dirección de proveedores con teléfono de la serie 445.
 
