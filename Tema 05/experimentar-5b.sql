1c670b6836cad80d4160fd55bff52e1fb65982c6
Jhoss <kuro.rc10@gmail.com>
2020-07-18 13:40:53 -0500

    Relizando los ejercicios hasta la pregunta 19

--- Tema 05/experimentar-5b.sql
+++ Tema 05/experimentar-5b.sql
@@ -45,46 +45,86 @@ GO
 
 -- 6. ¿Cómo se listan los clientes donde el cuarto y quinto carácter sea "02"?
 
+SELECT *
+FROM dbo.TB_CLIENTE AS CL
+WHERE CL.RUC_CLI LIKE '___02%'
+GO
 
 -- 7. ¿Cómo se convierte una cadena en minúscula (descripción de producto)?
 
+PRINT LOWER('HOLA')
+GO
+
+SELECT LOWER(P.DES_PRO) AS [DESCRIPCION]
+FROM dbo.TB_PRODUCTO AS P
+GO
 
 -- 8. ¿Cómo se convierte una cadena en mayúscula (apellido del vendedor)?
 
+PRINT UPPER('hola')
+GO
+
+SELECT UPPER(APE_VEN) AS [APELLIDO]
+FROM dbo.TB_VENDEDOR
+GO
 
 -- 9. ¿Cómo se reemplaza una cadena por otra cadena?
 
+PRINT REPLACE('hola', 'h', 'M')
+GO
 
 -- 10. ¿Cómo se reemplaza parte de una cadena y agrego otra?
 
+DECLARE @TEXT VARCHAR(100) = 'Bienvenido a SQL'
+PRINT REPLACE(@TEXT, 'Bienvenido', 'Hola')
+GO
 
 -- 11. ¿Cómo se muestra una cadena impresa al revés?
 
+DECLARE @TEXT VARCHAR(100) = 'Aprendiendo Sql Server'
+PRINT REVERSE(@TEXT)
+GO
 
 -- 12. ¿Cómo se repite varias veces una cadena?
 
+PRINT REPLICATE('HOLA',5)
+GO
 
 -- 13. ¿Cómo se obtiene el absoluto de un número?
 
+DECLARE @NUMBER INT = -10
+PRINT ABS(@NUMBER)
+GO
 
 -- 14. ¿Cómo se eleva un número a una potencia?
 
+DECLARE @NUMBER INT = 3
+PRINT SQUARE(@NUMBER)
+PRINT POWER(@NUMBER, 3)
+GO
 
 -- 15. ¿Cómo se redondea al número entero mayor más próximo?
 
+PRINT CEILING(10.27)
+PRINT CEILING(10.85)
 
 -- 16. ¿Cómo se redondea al número entero menor más próximo?
 
+PRINT FLOOR(10.27)
+PRINT FLOOR(10.85)
 
 -- 17. ¿Cómo se redondea un número indicándole la cantidad de decimales a
 --      considerar?
 
+PRINT ROUND(10.5852, 2)
 
 -- 18. ¿Cómo se convierte un tipo de número con 8 decimales a otro, de 2?
 
+PRINT ROUND(10.15545982, 2)
 
 -- 19. ¿Cómo se imprime un mensaje en la consola del SQL Server 2014?
 
+PRINT 'Bienvenido a la Consola'
 
 -- 20. ¿Cómo se da formato DD/MM/YY al campo fecha de una tabla?
 
