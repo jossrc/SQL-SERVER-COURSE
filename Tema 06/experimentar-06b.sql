f4b9da0d81a0ffcea5b31b7102266cc9e20b30c7
Jhoss <kuro.rc10@gmail.com>
2020-07-21 11:00:45 -0500

    Realizando los ejercicios hasta la penúltima pregunta

--- Tema 06/experimentar-06b.sql
+++ Tema 06/experimentar-06b.sql
@@ -56,26 +56,60 @@ JOIN dbo.TB_PROVEEDOR AS P
   ON D.COD_DIS = P.COD_DIS
 JOIN dbo.TB_ABASTECIMIENTO AS A
   ON P.COD_PRV = A.COD_PRV
-JOIN dbo.TB_PRODUCTO AS PR
-  ON A.COD_PRO = PR.COD_PRO
-WHERE PR.COD_PRO = 'P002'
+WHERE A.COD_PRO = 'P002'
 GO
 
 -- 6. Liste el código de cliente, razón social y distrito (incluir distritos donde no existen
 --    clientes)
 
+SELECT CL.COD_CLI AS [CODIGO CLIENTE],
+       CL.RAZ_SOC_CLI AS [RAZÓN SOCIAL],
+       D.NOM_DIS AS [DISTRITO]
+FROM dbo.TB_CLIENTE AS CL
+RIGHT JOIN dbo.TB_DISTRITO AS D
+  ON CL.COD_DIS = D.COD_DIS
+ORDER BY CL.COD_CLI ASC
+GO
 
 -- 7. Liste los distritos donde no existen clientes (utilice LEFT OUTER JOIN).
 
+SELECT D.NOM_DIS
+FROM dbo.TB_DISTRITO AS D
+LEFT OUTER JOIN dbo.TB_CLIENTE AS CL
+  ON D.COD_DIS = CL.COD_DIS
+WHERE CL.COD_CLI IS NULL
+ORDER BY D.NOM_DIS ASC
+GO
 
 -- 8. Liste los distritos donde no existen clientes (utilice RIGHT OUTER JOIN).
 
+SELECT D.NOM_DIS
+FROM dbo.TB_CLIENTE AS CL
+RIGHT OUTER JOIN dbo.TB_DISTRITO AS D
+  ON CL.COD_DIS = D.COD_DIS
+WHERE CL.COD_CLI IS NULL
+ORDER BY D.NOM_DIS ASC
+GO
 
 -- 9. Liste los proveedores (razón social y dirección), sin órdenes de compra.
 
+SELECT P.RAZ_SOC_PRV AS [RAZÓN SOCIAL],
+       P.DIR_PRV AS [DIRECCIÓN]
+FROM dbo.TB_PROVEEDOR AS P
+LEFT JOIN dbo.TB_ORDEN_COMPRA AS OC
+  ON P.COD_PRV = P.COD_PRV
+WHERE OC.NUM_OCO IS NULL
+GO
 
 -- 10. Liste los clientes que no han generado facturas.
 
+SELECT CL.*
+FROM dbo.TB_CLIENTE AS CL
+LEFT JOIN dbo.TB_FACTURA AS F
+  ON CL.COD_CLI = F.COD_CLI
+WHERE F.NUM_FAC IS NULL
+ORDER BY CL.COD_CLI ASC
+GO
 
 -- 11. Liste la cantidad de clientes sin facturas.
 
