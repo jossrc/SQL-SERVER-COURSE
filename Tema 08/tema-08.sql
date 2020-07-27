c18977fdd04a4527bbe0f493a029e99d10026712
Jhoss <kuro.rc10@gmail.com>
2020-07-27 14:17:50 -0500

    Variables globales

--- Tema 08/tema-08.sql
+++ Tema 08/tema-08.sql
@@ -1,2 +1,16 @@
 USE Negocios
 GO
+
+-- VARIABLES GLOBALES
+
+  -- La versión del SQL Server
+PRINT 'Versión: ' + @@version
+  -- Lenguaje configurado en el Servidor
+PRINT 'Lenguaje: ' + @@language
+  -- Nombre del Servidor
+PRINT 'Servidor: ' + @@servername
+  -- Nro. de conexiones permitidas
+PRINT 'Conexiones: ' + str(@@max_connections)
+GO
+
+
