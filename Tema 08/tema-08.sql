2830ffbeb638ce1b86c414033f63a20c22ecd3c9
Jhoss <kuro.rc10@gmail.com>
2020-07-27 14:27:41 -0500

    Variables locales

--- Tema 08/tema-08.sql
+++ Tema 08/tema-08.sql
@@ -13,4 +13,31 @@ PRINT 'Servidor: ' + @@servername
 PRINT 'Conexiones: ' + str(@@max_connections)
 GO
 
+-- VARIABLES LOCALES
+
+  -- Ejemplo 1
+DECLARE @v_price DECIMAL
+SET @v_price = 50
+
+SELECT *
+FROM Compras.productos AS P
+WHERE P.PrecioUnidad > @v_price
+GO
+
+  -- Ejemplo 2
+DECLARE @mx DECIMAL
+SELECT @mx = MAX(P.PrecioUnidad)
+FROM Compras.productos AS P
+
+PRINT 'El Precio más alto es: ' + STR(@mx)
+GO
+
+  -- Ejemplo 3
+DECLARE @v_mn DECIMAL
+SELECT @v_mn = MIN(P.PrecioUnidad)
+FROM Compras.productos AS P
+
+PRINT 'El precio más bajo es: ' + STR(@v_mn)
+GO
+
 
