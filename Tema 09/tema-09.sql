a5aea484a0c9fb3b87ec04c2a5da9f1f01487a3e
Jhoss <kuro.rc10@gmail.com>
2020-08-01 13:04:30 -0500

    Introducción a los cursores

--- Tema 09/tema-09.sql
+++ Tema 09/tema-09.sql
@@ -1,2 +1,19 @@
 USE Negocios
 GO
+
+-- MANEJO DE CURSORES
+
+-- 01. Declarar Cursor
+DECLARE mi_cursor CURSOR SCROLL FOR
+  SELECT * FROM Compras.productos
+-- 02. Abrir cursor
+OPEN mi_cursor
+-- 03. Navegar los registros solicitados
+FETCH FIRST FROM mi_cursor
+FETCH ABSOLUTE 6 FROM mi_cursor
+FETCH LAST FROM mi_cursor
+-- 04. Cerrar el cursor
+CLOSE mi_cursor
+-- 05. Liberar el cursor
+DEALLOCATE mi_cursor
+GO
