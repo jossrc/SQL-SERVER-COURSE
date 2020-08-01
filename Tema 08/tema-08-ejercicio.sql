f5929e4626fed3aaebaf23a9dff9ff8cc1bff602
Jhoss <kuro.rc10@gmail.com>
2020-08-01 10:34:56 -0500

    Iniciando el ejercicio del Tema 08

---
+++ Tema 08/tema-08-ejercicio.sql
@@ -0,0 +1,13 @@
+USE Negocios
+GO
+
+/* 
+   Efectúe una transacción explícita para registrar nuevos
+   Clientes, en caso el nombre del Cliente no se repita, se confirma
+   la transacción, de lo contrario, genere un error de severidad
+   grave, controlándolo con Try Catch y deshacer.
+
+   Controle los errores de duplicidad de llave primaria y llave
+   foránea..
+
+ */
