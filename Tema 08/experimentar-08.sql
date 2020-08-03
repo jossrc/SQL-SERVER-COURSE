657f0b507967ae3b0d2f02e1442eb75ab597daf4
Jhoss <kuro.rc10@gmail.com>
2020-08-03 11:46:18 -0500

    Realizando los ejercicios hasta la pregunta 5

--- Tema 08/experimentar-08.sql
+++ Tema 08/experimentar-08.sql
@@ -7,23 +7,75 @@ GO
 
 -- 1. Declare, asigne un valor e imprima el valor de una variable de tipo numérico.
 
+DECLARE @num INT
+SET @num = 10
+PRINT STR(@num)
+GO
 
 -- 2. Declare, asigne un valor e imprima la cantidad de Ordenes de Compra atendidas
 --    por el proveedor 'PR05'.
 
+DECLARE @codProvider VARCHAR(100),
+        @count INT
+SET @codProvider = 'PR05'
+SELECT @count = COUNT(NUM_OCO)  FROM dbo.TB_ORDEN_COMPRA AS [OC]
+WHERE OC.COD_PRV = @codProvider
+PRINT 'Cantidad Vendida : ' + STR(@count)
+GO
 
 -- 3. Declare, asigne un valor e imprima la deuda de un cliente (almacene el código del
 --    cliente) por concepto de facturas pendientes (estado de la factura = 1).
 
+DECLARE @codClient CHAR(4),
+        @debt SMALLMONEY
+SET @codClient = 'C019'
+SELECT @debt = SUM(DF.CAN_VEN * DF.PRE_VEN) FROM TB_FACTURA AS [F]
+INNER JOIN TB_DETALLE_FACTURA  AS [DF]
+  ON F.NUM_FAC = DF.NUM_FAC
+WHERE F.EST_FAC = 1 AND F.COD_CLI = @codClient
+GROUP BY F.COD_CLI, F.NUM_FAC
+PRINT 'DEUDA TOTAL : S/' + STR(@debt)
+GO
 
 -- 4. Declare, calcule e imprima el promedio de antigüedad de los clientes, si es mayor a
---    15 años imprima 'Cliente Fiel', de lo contario ‘Cliente Nuevo'.
+--    8 años imprima 'Cliente Fiel', de lo contario ‘Cliente Nuevo'.
 
+DECLARE @avg INT
+SET @avg = 8
+SELECT CL.RAZ_SOC_CLI AS [CLIENTE],
+       'ANTIGUEDAD' = (
+         CASE
+           WHEN DATEDIFF(YY,CL.FEC_REG, GETDATE()) > @avg THEN 'CLIENTE FIEL'
+           ELSE 'CLIENTE NUEVO'
+         END
+       )
+FROM TB_CLIENTE AS [CL]
+GO
 
 -- 5. Modifique el ejercicio anterior para que se calcule e imprima el promedio de
---    antigüedad de los clientes, si es mayor de 15 años imprima 'Clientela Fiel' y su límite
+--    antigüedad de los clientes, si es mayor de 8 años imprima 'Clientela Fiel' y su límite
 --    de crédito será 2500 soles, pero de lo contario, imprima 'Clientela Nueva' con un
 --    límite de crédito de 1000 soles.
+
+DECLARE @avg INT
+SET @avg = 8
+SELECT CL.RAZ_SOC_CLI AS [CLIENTE],
+       'ANTIGUEDAD' = (
+        CASE
+          WHEN DATEDIFF(YY,CL.FEC_REG, GETDATE()) > @avg AND
+               SUM(DF.CAN_VEN * DF.PRE_VEN) > 2500
+            THEN 'CLIENTELA FIEL'
+          WHEN SUM(DF.CAN_VEN * DF.PRE_VEN) > 1000
+            THEN 'CLIENTELA NUEVA'
+        END
+      )
+FROM TB_CLIENTE AS [CL]
+JOIN TB_FACTURA AS [F]
+  ON CL.COD_CLI = F.COD_CLI
+JOIN TB_DETALLE_FACTURA AS [DF]
+  ON F.NUM_FAC = DF.NUM_FAC
+GROUP BY CL.RAZ_SOC_CLI, DATEDIFF(YY,CL.FEC_REG, GETDATE())
+GO
 
 /*
    6. Declare, y determine la siguiente regla de negocios para una determinada factura:
