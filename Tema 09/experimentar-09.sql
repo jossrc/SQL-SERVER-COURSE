19ee9f22d840ec902f9e76fdd9da1f0dc96a6daa
Jhoss <kuro.rc10@gmail.com>
2020-08-04 13:56:04 -0500

    Iniciando el experimentar 09

---
+++ Tema 09/experimentar-09.sql
@@ -0,0 +1,27 @@
+USE VENTASCIB
+GO
+
+/* 
+  01. Cree un curso que permita mostrar los datos del Proveedor 
+      y el nombre de los productos y su precio que cada uno abastece.
+      Crear 02 soluciones:
+       a. Solución 2a: con consultas combinadas
+       b. Solución 3b: con cursores anidados
+*/
+
+-- 02. Cree un cursor que genere un reporte que muestre el nombre del país
+--   y el producto que más consumen, así como también el menos consumido.
+
+
+-- 03. Cree un cursor que muestre el n° de factura, nombre de cliente,
+--     nombre del vendedor, nombre del producto, cantidad, 
+--     precio unitario, subtotal y total.
+
+/*
+   04. Cree un cursor que permita insertar el código, ruc, 
+    monto de la deuda y mora de los clientes en la tabla Tb_Morosos
+    (qué debe crearse previamente), considerar los siguientes criterios:
+      + Si es menos a 200 soles será del 10% de la deuda.
+      + Si es mayor igual a 200 y menos a 1000, será del 15% de la deuda.
+      + Si es mayor a 1000 será del 20% de la deuda.
+*/
